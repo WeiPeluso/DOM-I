11b7bd8f2f53cc9d6c897721460b8613539a4757
wei peluso <weipeluso@gmail.com>
2020-05-04 18:45:01 -0400

    finished stretch project

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -11,32 +11,39 @@ msTens.textContent="0";
 let msTensNumber=0;
 let secondOnesNumber=0;
 let secondTensNumber=0;
+let counter=0;
 
 var timer = setInterval(function(){
+    counter++;
 
-    if(msTensNumber===9){
+    /*if(msTensNumber===9){
         msTensNumber=0;
         secondOnesNumber=secondOnesNumber+1;
     }
-    else{
+    else{*/
        msTensNumber=msTensNumber+1;
+    //}
+    
+    if(counter%10===0){
+        msTensNumber=0;
+        secondOnesNumber=secondOnesNumber+1;
     }
 
-    if(secondOnesNumber===9){
+    if(counter%100===0){
         secondOnesNumber=0;
         secondTensNumber=secondTensNumber+1;
     }
 
-    secondTens.textContent=secondTensNumber;
-    secondOnes.textContent=secondOnesNumber;
-    msHundres.textContent=msTensNumber;
-
-
-    if(secondTensNumber===1){
+    if(counter===100){
         secondTens.style.color="red";
         secondOnes.style.color="red";
         msHundres.style.color="red";
         msTens.style.color="red";
         clearInterval(timer);
     }
+
+    secondTens.textContent=secondTensNumber;
+    secondOnes.textContent=secondOnesNumber;
+    msHundres.textContent=msTensNumber;
+
     },100);
